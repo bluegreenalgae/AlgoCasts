5d0949af213bf9abcd9969fcaff2ccea0e6589a7
Randall <trzaskarandall@gmail.com>
2019-12-25 12:13:16 -0500

    complete palindrome exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,8 +6,13 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+////// Solution 2 //////////
 function reverse(str) {
-  return str.split('').reverse().join('');
+  let reversed = '';
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
 }
 
 module.exports = reverse;
