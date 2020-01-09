4de1fe394c93da56cd57337ce714530f97f08d79
Randall <trzaskarandall@gmail.com>
2020-01-09 17:04:30 +0700

    chunk first attempt

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,26 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+// set chunk size
+
+// if array.length is longer than chunk size: create new subarray
+if (array.length > size) {
+  var newArray = [];
+  for (i = 0; i < size; i++) {
+    newArray.push(array[i]);
+    var iCount = i;
+    if (iCount > size) {
+      var newArray[iCount] = [];
+    }
+  }
+
+} else {
+  return array;
+}
+// insert item into new subarray until subarray.length is equal to chunk size, then create new subarray
+
+
+}
 
 module.exports = chunk;
