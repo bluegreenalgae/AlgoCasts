cd8a035f4a5c9a2491131e4603b0d2fbf58ffbeb
Randall <trzaskarandall@gmail.com>
2020-01-10 20:47:10 +0700

    complete chunk alternative solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,25 +9,38 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-  // create empty array to hold chunks
   const chunked = [];
+  var index = 0;
 
-  // for each element in unchuncked array
-  for (let element of array) {
-    // retrieve last element in chuncked
-    const last = chunked[chunked.length - 1];
-
-    // if last element does not exist or equals chunked size
-    if (!last || last.length === size) {
-      // push a new element in to chunked with current element
-      chunked.push([element]);
-    } else {
-      // else add the current element to current chunked
-      last.push(element);
-    }
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
   }
 
   return chunked;
 }
 
 module.exports = chunk;
+
+
+// function chunk(array, size) {
+//   // create empty array to hold chunks
+//   const chunked = [];
+//
+//   // for each element in unchuncked array
+//   for (let element of array) {
+//     // retrieve last element in chuncked
+//     const last = chunked[chunked.length - 1];
+//
+//     // if last element does not exist or equals chunked size
+//     if (!last || last.length === size) {
+//       // push a new element in to chunked with current element
+//       chunked.push([element]);
+//     } else {
+//       // else add the current element to current chunked
+//       last.push(element);
+//     }
+//   }
+//
+//   return chunked;
+// }
