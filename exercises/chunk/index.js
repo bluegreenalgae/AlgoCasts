8f7f0fd0f031e3a33322e1266d376dd5d7d4ed20
Randall <trzaskarandall@gmail.com>
2020-01-10 20:34:22 +0700

    complete chunk exercise

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,25 +9,25 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-// set chunk size
+  // create empty array to hold chunks
+  const chunked = [];
 
-// if array.length is longer than chunk size: create new subarray
-if (array.length > size) {
-  var newArray = [];
-  for (i = 0; i < size; i++) {
-    newArray.push(array[i]);
-    var iCount = i;
-    if (iCount > size) {
-      var newArray[iCount] = [];
+  // for each element in unchuncked array
+  for (let element of array) {
+    // retrieve last element in chuncked
+    const last = chunked[chunked.length - 1];
+
+    // if last element does not exist or equals chunked size
+    if (!last || last.length === size) {
+      // push a new element in to chunked with current element
+      chunked.push([element]);
+    } else {
+      // else add the current element to current chunked
+      last.push(element);
     }
   }
 
-} else {
-  return array;
-}
-// insert item into new subarray until subarray.length is equal to chunk size, then create new subarray
-
-
+  return chunked;
 }
 
 module.exports = chunk;
