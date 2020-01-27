9c9c3c2c1d1fc5faaaaf7fee0edcff4e6bd7da47
Randall <trzaskarandall@gmail.com>
2020-01-27 12:46:52 +0700

    complete pyramid iterative solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,23 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+
+// row width = n + (n-1)
+
+function pyramid(n) {
+  const midpoint = Math.floor((2 * n - 1)/2);
+
+  for (let row = 0; row < n; row++) {
+    level = '';
+
+    for (let col = 0; col < 2n - 1; col++) {
+      if (midpoint - row <= col && midpoint + row >= col) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
+
+}
 
 module.exports = pyramid;
