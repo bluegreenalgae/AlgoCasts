55b30312c97afeab92901e721c6621fb00c0a575
Randall <trzaskarandall@gmail.com>
2020-01-27 13:00:27 +0700

    complete pyramid recursive solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -17,20 +17,43 @@
 
 // row width = n + (n-1)
 
-function pyramid(n) {
-  const midpoint = Math.floor((2 * n - 1)/2);
+function pyramid(n, row = 0, level = '') {
 
-  for (let row = 0; row < n; row++) {
-    level = '';
+  if (row === n) {
+    return;
+  }
 
-    for (let col = 0; col < 2n - 1; col++) {
-      if (midpoint - row <= col && midpoint + row >= col) {
-        level += '#';
-      } else {
-        level += ' ';
-      }
-    }
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
 
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
 }
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//
+//   for (let row = 0; row < n; row++) {
+//     level = '';
+//
+//     for (let col = 0; col < 2 * n - 1; col++) {
+//       if (midpoint - row <= col && midpoint + row >= col) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
