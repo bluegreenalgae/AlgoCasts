7005b4044c3a29b5f34eb56bb667932f392639ca
Randall <trzaskarandall@gmail.com>
2020-01-29 11:38:34 +0700

    first attempt regex solution to vowels in python

--- exercises/vowels/vowels.py
+++ exercises/vowels/vowels.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 
 def vowels(str):
-    vowel = ['a','e','i','o','u']
-    count = 0;
+    matches = str.match(/[aeiou]/gi)
+    if matches:
+        return len(matches)
+    else:
+        return 0
 
-    for letter in str.lower():
-        if letter in vowel:
-            count++
 
-    print(count)
+
+
+
+# def vowels(str):
+#     vowel = ['a','e','i','o','u']
+#     count = 0;
+#
+#     for letter in str.lower():
+#         if letter in vowel:
+#             count++
+#
+#     print(count)
